f6861baaf84ea444f9b684b745f1d689b455da7c
Ivan Radko <118024148+RadkoIvanUa@users.noreply.github.com>
2023-06-20 23:52:22 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
